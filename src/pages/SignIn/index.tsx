c0fb7c1a9c2afc21e117efebe047769e0840aa74
Diego Souza <dee-souza@hotmail.com>
2020-04-29 10:14:39 -0300

    :lipstick: Create button and input components with styles

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Entrar');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignUp: React.FC = () => <Container />;
+import logImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="name" icon="user" placeholder="Nome" />
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Entrar');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
 
 export default SignUp;
