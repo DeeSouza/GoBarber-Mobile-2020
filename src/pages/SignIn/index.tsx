8e8166008a1bf18bda7ecbe5f72f2777ce9ae049
Diego Souza <dee-souza@hotmail.com>
2020-07-03 15:12:13 -0300

    :white_check_mark: Update test signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -39,7 +39,7 @@ interface SignInFormData {
 }
 
 const SignIn: React.FC = () => {
-  const { signIn, user } = useAuth();
+  const { signIn } = useAuth();
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
